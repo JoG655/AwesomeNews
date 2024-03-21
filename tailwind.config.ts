e12c644dade6d9d6d752dc1b01f1fcf4b17e7101
JoG655 <gregov.josip@gmail.com>
2024-03-21 18:18:38 +0100

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        focus: {
+          DEFAULT: "#cbd5e1",
+        },
+        primary: {
+          DEFAULT: "#ffffff",
+          50: "#fafafa",
+          100: "#f5f5f5",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+          950: "#0a0a0a",
+        },
       },
     },
   },
